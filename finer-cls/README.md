2d837bdd24ac95a057b371b7503d8d2052a23c16
vidit09 <vidit7@outlook.com>
2018-03-19 22:55:45 +0100

    added finer classes

---
+++ finer-cls/README.md
@@ -0,0 +1,9 @@
+# VLAD based matching
+
+## Steps to use.
+
+1. Download Grocery Product dataset.
+2. Run vlad_sift.m to generate VLAD for images
+3. Need output of Faster RCNN to get finer label with match_des.m
+
+
--- finer-cls/vlad_sift.m
+++ finer-cls/vlad_sift.m
@@ -1,39 +1,53 @@
-function VladSift()
-    phow = 1;
+%%
+% Compute VLAD descriptor out of dense color SIFT
+function vlad_sift()
+    
+    % Number of Vocabulary Words
     num_clusters = 500;
+    % Set path to Grocery Product directory
+    train_image_dir = '';
 
-    dirname = ['vlad_phsift_out_' num2str(num_clusters) extra];
-    mkdir(dirname);
+    % Output Directory
+    outdirname = ['vlad_phsift_out_' num2str(num_clusters) extra];
+    mkdir(outdirname);
     
-    cat = textread('cat_mapping.txt','%s');
-    all_images = textread('TrainingFiles.txt','%s');
+    cat = textread([train_image_dir 'cat_mapping.txt'],'%s');
+    all_images = textread([train_image_dir 'TrainingFiles.txt'],'%s');
     
+    % Calculate cluster per class and vlad
     for i = 1:length(cat)
        disp(['For category:' cat{i}]);
        index = find(contains(all_images,cat{i}));
-       [enc_,centers_] = descriptor(index,all_images,num_clusters,phow);
+       [enc_,centers_] = descriptor(index,all_images,num_clusters,train_image_dir);
+       
+       % Save the descriptor and kmeans centers
        enc = enc_;
        centers = centers_;
-       save([dirname '/vlad_kmeans' num2str(i) '.mat'],'enc','centers');
+       save([outdirname '/vlad_kmeans' num2str(i) '.mat'],'enc','centers');
     end    
         
 end
 
-function [enc,centers] = descriptor(index,all_images,num_clusters,phow)
+%%
+% Get vlad descriptor for all images in a category
+function [enc,centers] = descriptor(index,all_images,num_clusters,dir_path)
+    
     disp('Extract SIFT ...');
     for i = 1:length(index)
-        im_name = all_images{index(i)}
+        im_name = all_images{index(i)};
         [path,name,ext] = fileparts(im_name);
-        im_name = [path '/' name '_bkg_reduced.jpg'];
+        im_name = [dir_path path '/' name '_bkg_reduced.jpg'];
         [c,sift{i}] = phow_sift(im_name);
     end
     
+    %Cluster the SIFTs
     disp('Get Kmeans Centers ...');
     all_sift = single(cell2mat(sift));
     centers = vl_kmeans(all_sift, num_clusters,'Initialization', 'plusplus');
     
     kdtree = vl_kdtreebuild(centers) ;
     
+    %Computer VLAD per image
     disp('Get VLAD ...');
     for i = 1:length(index)
         d = single(sift{i});
@@ -45,7 +59,8 @@ function [enc,centers] = descriptor(index,all_images,num_clusters,phow)
         enc{i} = vl_vlad(d,centers,single(assignments),'NormalizeComponents');
     end
 end
-
+%%
+%Compute dense color SIFT
 function [f,sf] = phow_sift(im_name)
     im = single(imread(im_name)); 
     [f,sf] = vl_phow(im,'sizes',[8,16,24,32],'step',6,'Color','rgb');
